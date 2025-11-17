b5d09c926ae71f8847939d4acac0b3d05a6ba3f8
Std1o <37378410+Std1o@users.noreply.github.com>
2025-11-17 15:37:24 +0300

    upd databse

--- src/vafs/settings.py
+++ src/vafs/settings.py
@@ -1,9 +1,17 @@
 from pydantic_settings import BaseSettings
+import os
 
 class Settings(BaseSettings):
     server_host: str = '0.0.0.0'
     server_port: int = 80
-    database_url: str = 'sqlite:///../../database.sqlite3'
+
+    @property
+    def database_url(self) -> str:
+        # Если запущено в Docker
+        if os.path.exists('/.dockerenv'):
+            return 'sqlite:///./database.sqlite3'
+        else:
+            return 'sqlite:///../../database.sqlite3'
 
     jwt_sercret: str = 'I9b1WCWByTM9D6DOIDi-yTRQsieCZgNAaUzdaqwqc_Q'
     jwt_algorithm: str = 'HS256'
